40f12c3c4924c71b06d49df5d4d6197837369aac
harishkumarforever <harishking809@gmail.com>
2023-01-27 06:54:13 +0530

    smll

--- src/components/Route/routes/Container.tsx
+++ src/components/Route/routes/Container.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/Header/Header";
-import Nav from "../components/Nav/Nav";
+import Header from "../../Header/Header";
+import Nav from "../../Nav/Nav";
 // import Header from "../components/Header/Header";
 // import Sidebar from "../components/Sidebar/Sidebar";
 import "./Container.scss";
--- src/components/Route/routes/PublicRoute.js
+++ src/components/Route/routes/PublicRoute.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { isLogin } from "../utils";
-import { DASHBOARD_ROUTE } from "../utils/constant";
+import { DASHBOARD_ROUTE } from "../../../utils/constant";
 
 const PublicRoute = ({ component: Component, ...rest }) => {
   return isLogin() ? (
